7a22ede5cbeebc534afccf126a83f0125f9c60ec
LAZ-R <crazydrawer62@gmail.com>
2023-11-29 16:44:03 +0100

    v 0.0.5 : ajout s2e11 s2e12 s2e13

--- js/data.js
+++ js/data.js
@@ -50,9 +50,10 @@ export const CHARACTERS = [
 ];
 
 export const QUOTES = [
+  // S2 E5 --------------------------------------------------------------------
   {
     id: 1,
-    character_id: 1,
+    character_id: 1, // Shredder
     quote: `"Espèce d'accident génétique !"`,
     season: '2',
     episode: '5',
@@ -60,7 +61,7 @@ export const QUOTES = [
   },
   {
     id: 2,
-    character_id: 3,
+    character_id: 3, // Leonardo
     quote: `"Eh ben c'est une longue histoire...<br>— Alors vous la raconterez un autre jour !"`,
     season: '2',
     episode: '5',
@@ -68,39 +69,43 @@ export const QUOTES = [
   },
   {
     id: 3,
-    character_id: 2,
+    character_id: 2, // Raphael
     quote: `"Je connais très bien le responsable du musée d'histoire naturelle.<br>— ... Et alors ?"`,
     season: '2',
     episode: '5',
     timer: '00:18:11',
   },
+  // S2 E6 --------------------------------------------------------------------
   {
     id: 4,
-    character_id: 4,
+    character_id: 4, // Rocksteady
     quote: `"Oh, t'as déjà vu une créature aussi laide ?<br>— Oh oui alors, c'est tout à fait ton ancienne petite amie, on dirait sa soeur !"`,
     season: '2',
     episode: '6',
     timer: '00:15:37',
   },
+  // S2 E7 --------------------------------------------------------------------
   {
     id: 5,
-    character_id: 6,
+    character_id: 6, // Michelangelo
     quote: `"Oh mais c'est vraiment un rat-mulot celui-là !"`,
     season: '2',
     episode: '7',
     timer: '00:17:58',
   },
+  // S2 E8 --------------------------------------------------------------------
   {
     id: 6,
-    character_id: 7,
+    character_id: 7, // Krang
     quote: `"Mais qu'est-ce qu'il y a, sautrelle malfaisante ?"`,
     season: '2',
     episode: '8',
     timer: '00:10:29',
   },
+  // S2 E9 --------------------------------------------------------------------
   {
     id: 7,
-    character_id: 6,
+    character_id: 6, // Michelangelo
     quote: `"Je viens juste de voir les lieutenants de Shredder à la bibliothèque municipale.<br>— Quoi ? Les deux caricatures ? Ces deux débiles ne savent même pas lire !"`,
     season: '2',
     episode: '9',
@@ -108,7 +113,7 @@ export const QUOTES = [
   },
   {
     id: 8,
-    character_id: 1,
+    character_id: 1, // Shredder
     quote: `"Si je vous répondais, vous n'y comprendriez absolument rien de toute façon, alors pourquoi perdre du temps pour rien ?"`,
     season: '2',
     episode: '9',
@@ -116,7 +121,7 @@ export const QUOTES = [
   },
   {
     id: 9,
-    character_id: 2,
+    character_id: 2, // Raphael
     quote: `"En plus d'être idots et laids, ce sont aussi des lâches..."`,
     season: '2',
     episode: '9',
@@ -124,7 +129,7 @@ export const QUOTES = [
   },
   {
     id: 10,
-    character_id: 7,
+    character_id: 7, // Krang
     quote: `"C'est encore toi moucheron stupide..."`,
     season: '2',
     episode: '9',
@@ -132,7 +137,7 @@ export const QUOTES = [
   },
   {
     id: 11,
-    character_id: 7,
+    character_id: 7, // Krang
     quote: `"Ah oui, coléoptère venimeux ?"`,
     season: '2',
     episode: '9',
@@ -140,15 +145,16 @@ export const QUOTES = [
   },
   {
     id: 12,
-    character_id: 7,
+    character_id: 7, // Krang
     quote: `"Envoyez moi tout de suite le neuro-brouilleur que j'ai inventé !<br>— ... Pourquoi faire ?"`,
     season: '2',
     episode: '9',
     timer: '00:12:26',
   },
+  // S2 E10 -------------------------------------------------------------------
   {
     id: 13,
-    character_id: 8,
+    character_id: 8, // REX-1
     quote: `"Si vous vouliez que je reste tranquille, il ne fallait pas me faire lever."`,
     season: '2',
     episode: '10',
@@ -156,7 +162,7 @@ export const QUOTES = [
   },
   {
     id: 14,
-    character_id: 8,
+    character_id: 8, // REX-1
     quote: `"La police c'est moi. Je vous arrête pour arrestation intempestive !"`,
     season: '2',
     episode: '10',
@@ -164,7 +170,7 @@ export const QUOTES = [
   },
   {
     id: 15,
-    character_id: 3,
+    character_id: 3, // Leonardo
     quote: `"On va vous désintégrer vos circuits intégrés !"`,
     season: '2',
     episode: '10',
@@ -172,10 +178,101 @@ export const QUOTES = [
   },
   {
     id: 16,
-    character_id: 8,
+    character_id: 8, // REX-1
     quote: `"J'n'aurai jamais dû quitter le Texas, ma mère avait raison."`,
     season: '2',
     episode: '10',
     timer: '00:18:45', 
+  },
+  // S2 E11 -------------------------------------------------------------------
+  {
+    id: 17,
+    character_id: 7, // Krang
+    quote: `"Des inconnus sont entrés dans le Technodrome, sauterelle boiteuse !"`,
+    season: '2',
+    episode: '11',
+    timer: '00:04:14', 
+  },
+  {
+    id: 18,
+    character_id: 5, // Donatello
+    quote: `"Est-ce que je suis à mon avantage ?<br>— Oh t'es pas pire que d'habitude."`,
+    season: '2',
+    episode: '11',
+    timer: '00:06:19', 
+  },
+  {
+    id: 19,
+    character_id: 7, // Krang
+    quote: `"Je vais t'aider, puceron verdâtre..."`,
+    season: '2',
+    episode: '11',
+    timer: '00:13:43', 
+  },
+  // S2 E12 -------------------------------------------------------------------
+  {
+    id: 20,
+    character_id: 3, // Leonardo
+    quote: `"Tu vas voir de quel bois j'me chauffe, Frankenstein du pauvre !"`,
+    season: '2',
+    episode: '12',
+    timer: '00:02:39', 
+  },
+  {
+    id: 21,
+    character_id: 6, // Michelangelo
+    quote: `"Merci, pouce, euh... mi-temps, euh... temps mort, on se calme.<br>— Ça vous ennuierait de nous repasser votre cassette à une vitesse normale qu'on comprenne quelque chose ?"`,
+    season: '2',
+    episode: '12',
+    timer: '00:12:59', 
+  },
+  {
+    id: 22,
+    character_id: 7, // Krang
+    quote: `"Alors sauterelle boiteuse, tu as enfin réussi à faire quelque chose convenablement ?"`,
+    season: '2',
+    episode: '12',
+    timer: '00:16:16', 
+  },
+  {
+    id: 23,
+    character_id: 7, // Krang
+    quote: `"Scarabée boursouflé, qu'est-ce qu'il s'passe ? Tu veux que je t'envoie un réparateur ? Tu veux aussi que je vienne réparer ta machine à laver ? N'hésite surtout pas ! Imbécile..."`,
+    season: '2',
+    episode: '12',
+    timer: '00:16:25', 
+  },
+  // S2 E13 -------------------------------------------------------------------
+  {
+    id: 24,
+    character_id: 2, // Raphael
+    quote: `"Il suffirait de pouvoir arrêter le fleuve, la centrale ne produirait plus aucune énergie.<br>— Oui ! Très bonne idée, on va trouver 50 millions de castors et on leur demandera de bâtir un barage !"`,
+    season: '2',
+    episode: '13',
+    timer: '00:09:08', 
+  },
+  {
+    id: 25,
+    character_id: 5, // Donatello
+    quote: `"Il a les moyen de faire ça ?<br>— Si vraiment tu veux une réponse claire et nette, oui."`,
+    season: '2',
+    episode: '13',
+    timer: '00:15:56', 
+  },
+  {
+    id: 26,
+    character_id: 2, // Raphael
+    quote: `"Allez tous vous abriter, cet explosif spécial a une puissance phénoménale !<br>— ... Ah oui, oui, c'est phénoménal. Enfin un explosif qui n'explose pas..."`,
+    season: '2',
+    episode: '13',
+    timer: '00:16:49', 
+  },
+  {
+    id: 27,
+    character_id: 7, // Krang
+    quote: `"Où est-tu Shredder ? C'est ta faute tout ça, coléoptère baveux. Tu m'le paiera, tu entends Shredder ?"`,
+    season: '2',
+    episode: '13',
+    timer: '00:20:35', 
   },
 ];
